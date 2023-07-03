5147c634073800e07a83fe282cedc79a865c145d
Shadows-kk <1213099875@qq.com>
2023-07-03 11:02:01 +0800

    feat: 路由跳转

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home: FC = () => {
+  const nav = useNavigate()
+
   return (
     <>
       <div>home</div>
