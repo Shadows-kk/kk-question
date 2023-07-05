471eaf44cbc797b3991329740b7b95f95667fc43
Shadows-kk <1213099875@qq.com>
2023-07-05 15:37:45 +0800

    feat: 注册页UI

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,47 @@
 import React, { FC } from 'react'
+import styles from './Register.module.scss'
+import { Link } from 'react-router-dom'
+import { Typography, Space, Form, Input, Button } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import { LOGIN_PATHNAME } from '../router'
 
+const { Title } = Typography
 const Register: FC = () => {
   return (
-    <>
-      <div>Register</div>
-    </>
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title>
+            <UserOutlined></UserOutlined>
+          </Title>
+          <Title level={2}>注册用户</Title>
+        </Space>
+      </div>
+      <div>
+        <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+          <Form.Item label="用户名" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="密码" name="password">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="确认密码" name="confirm">
+            <Input.Password />
+          </Form.Item>
+          <Form.Item label="昵称" name="nickname">
+            <Input />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                注册
+              </Button>
+              <Link to={LOGIN_PATHNAME}>已有账号去登录</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 export default Register
