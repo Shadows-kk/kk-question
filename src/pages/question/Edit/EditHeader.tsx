bd0d451dc5aed3263655a4b9e510886003a1a6f0
Shadows-kk <1213099875@qq.com>
2023-09-18 17:06:15 +0800

    feat: 修改问卷标题

--- src/pages/question/Edit/EditHeader.tsx
+++ src/pages/question/Edit/EditHeader.tsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Button, Typography, Space, Form, Input } from 'antd'
+import { LeftOutlined, EditOutlined } from '@ant-design/icons'
+import { useDispatch } from 'react-redux'
+import { changePageTitle } from '@/store/pageInfoReducer'
 import style from './EditHeader.module.scss'
-import { Button, Typography, Space } from 'antd'
-import { LeftOutlined } from '@ant-design/icons'
 import EditToolbar from './EditToolbar'
+import useGetPageInfo from '@/hooks/useGetPageInfo'
+
+const { Title } = Typography
+
+// 显示及修改标题组件
+const TitleElem: React.FC = () => {
+  const { title } = useGetPageInfo()
+  const dispatch = useDispatch()
+
+  const [editFlag, setEditFlag] = useState(false)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value.trim()
+    if (newValue) {
+      dispatch(changePageTitle({ title: newValue }))
+    }
+  }
+  if (editFlag) {
+    return (
+      <Input
+        value={title}
+        onPressEnter={() => {
+          setEditFlag(false)
+        }}
+        onBlur={() => {
+          setEditFlag(false)
+        }}
+        onChange={handleChange}
+      />
+    )
+  }
+  return (
+    <Space>
+      <Title>{title}</Title>
+      <Button
+        icon={<EditOutlined />}
+        type="text"
+        onClick={() => {
+          setEditFlag(true)
+        }}
+      ></Button>
+    </Space>
+  )
+}
+// 编辑器头部组件
 const EditHeader: React.FC = () => {
   const nav = useNavigate()
-  const { Title } = Typography
+
   return (
     <div className={style['header-wrapper']}>
       <div className={style['header']}>
@@ -15,7 +61,7 @@ const EditHeader: React.FC = () => {
             <Button type="link" icon={<LeftOutlined />} onClick={() => nav(-1)}>
               返回
             </Button>
-            <Title>问卷标题</Title>
+            <TitleElem></TitleElem>
           </Space>
         </div>
         <div className={style['center']}>
