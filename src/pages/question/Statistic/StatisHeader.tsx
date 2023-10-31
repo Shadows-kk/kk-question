df242ff83eaec482317e6ce994d4fff9b27004af
Shadows-kk <1213099875@qq.com>
2023-10-31 15:22:36 +0800

    feat: 使用useMemo优化StatisHeader组件

--- src/pages/question/Statistic/StatisHeader.tsx
+++ src/pages/question/Statistic/StatisHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useMemo } from 'react'
 import { Space, Button, Typography, Input, Tooltip, message, Popover } from 'antd'
 import type { InputRef } from 'antd'
 import { LeftOutlined, CopyOutlined, QrcodeOutlined } from '@ant-design/icons'
@@ -14,7 +14,33 @@ const StatisHeader: React.FC = () => {
   const { title, isPublished } = useGetPageInfo()
   // 生成链接和二维码
   const linkInputRef = useRef<InputRef>(null)
-  const genLinkAndQRcode = () => {
+  // const genLinkAndQRcode = () => {
+  //   if (!isPublished) return null
+  //   // 拼接url，需要参考c端的规则
+  //   const url = `http://localhost:3000/question/${id}`
+  //   const copyLink = () => {
+  //     const ele = linkInputRef.current
+  //     if (ele == null) return
+  //     ele.select()
+  //     document.execCommand('copy')
+  //     message.success('复制成功')
+  //   }
+  //   // 生成二维码
+  //   const QRCodeEle = <QRCode value={url} size={150} renderAs="canvas" />
+  //   return (
+  //     <Space>
+  //       <Input value={url} style={{ width: '300px' }} ref={linkInputRef}></Input>
+  //       <Tooltip title="复制链接">
+  //         <Button icon={<CopyOutlined />} onClick={copyLink}></Button>
+  //       </Tooltip>
+  //       <Popover content={QRCodeEle}>
+  //         <Button icon={<QrcodeOutlined />}></Button>
+  //       </Popover>
+  //     </Space>
+  //   )
+  // }
+  // 使用useMemo优化。考虑点：1.依赖项是否经常变化，变化不频繁可用；2.缓存的元素是否创建成本过高。
+  const LinkAndQRcode = useMemo(() => {
     if (!isPublished) return null
     // 拼接url，需要参考c端的规则
     const url = `http://localhost:3000/question/${id}`
@@ -38,7 +64,7 @@ const StatisHeader: React.FC = () => {
         </Popover>
       </Space>
     )
-  }
+  }, [id, isPublished])
   return (
     <div className={style['header-wrapper']}>
       <div className={style['header']}>
@@ -50,7 +76,7 @@ const StatisHeader: React.FC = () => {
             <Title>{title}</Title>
           </Space>
         </div>
-        <div className={style['center']}>{genLinkAndQRcode()}</div>
+        <div className={style['center']}>{LinkAndQRcode}</div>
         <div className={style['right']}>
           <Button
             type="primary"
