e66f2a0a8d2c25e849d1d6888abeeecb66a5a041
Shadows-kk <1213099875@qq.com>
2023-11-01 14:42:51 +0800

    feat: 配置webpack，合理使用缓存，抽离antd与react-dom进行分包

--- craco.config.js
+++ craco.config.js
@@ -13,5 +13,35 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure(webpackConfig) {
+      if (webpackConfig.mode == 'production') {
+        // 抽离公共代码，只在生产环境下
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all', //所有模块
+          cacheGroups: {
+            antd: {
+              name: 'antd-chunk',
+              priority: 100,
+              test: /antd/,
+            },
+            reactDom: {
+              name: 'reactDom-chunk',
+              priority: 99,
+              test: /react-dom/,
+            },
+            // 其他的第三方模块
+            vendors: {
+              name: 'vendors-chunk',
+              priority: 98,
+              test: /node_modules/,
+            },
+          },
+        }
+      }
+      return webpackConfig
+    },
   },
 }
